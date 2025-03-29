3a556f3a9a8a91e7ccd076e114fe431a64583b11
CodeCreators <134051118+TheCodeCreators1@users.noreply.github.com>
2025-03-29 20:23:07 +0430

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,19 @@
+// Select the login form element
+
+
+// Select the signup form element
+
+
+// Select the login button (label element)
+
+
+// Select the signup button (label element)
+
+
+// Select the signup link inside the .signup-link container
+
+
+// Select the title text element that will be animated
+
+// Select the container for the forms to apply transformations
+
